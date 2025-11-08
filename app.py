d73f36e42ef03de9ad0344195412fdddeb2baa73
Bhanusai03 <bhanusaikollipara@gmail.com>
2025-11-08 12:07:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,8 @@ class Student(db.Model):
     student_class = db.Column(db.String(20), nullable=False)
     subject = db.Column(db.String(100), nullable=False)
     grade = db.Column(db.String(5), nullable=False)
-
+ with app.app_context():
+        db.create_all()
 # Home route — ✅ Fetch data from DB
 @app.route("/")
 def home():
@@ -81,6 +82,6 @@ def update_student(id):
     return render_template("update_student.html", student=student)
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
+   
     app.run(debug=True)
+
